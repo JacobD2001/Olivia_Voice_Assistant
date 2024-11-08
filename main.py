65a7dbbfb3398503321a6f063e30d45c79659c51
JacobD2001 <dzikowskijakub2001@gmail.com>
2024-11-08 17:09:29 +0100

    main models changed

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
 import asyncio
+
 from dotenv import load_dotenv
 from livekit.agents import AutoSubscribe, JobContext, WorkerOptions, cli, llm
 from livekit.agents.voice_assistant import VoiceAssistant
-from livekit.plugins import openai, silero
+from livekit.plugins import openai, silero, deepgram, cartesia
+from api import AssistantFnc
 
 load_dotenv()
+
 
 async def entrypoint(ctx: JobContext):
     initial_ctx = llm.ChatContext().append(
         role="system",
         text=(
-            "You are a helpful assistant. You are here to help the user with their questions."
-            "Be friendly and helpful."
+            "You are a voice assistant created by LiveKit. Your interface with users will be voice. "
+            "You should use short and concise responses, and avoiding usage of unpronouncable punctuation."
         ),
     )
     await ctx.connect(auto_subscribe=AutoSubscribe.AUDIO_ONLY)
+    fnc_ctx = AssistantFnc()
 
-    assistant = VoiceAssistant(
+    assitant = VoiceAssistant(
         vad=silero.VAD.load(),
-        stt=openai.STT(),
-        llm=openai.LLM(),
-        tts=openai.TTS(),
+        stt=deepgram.STT(),
+        llm=openai.LLM.with_groq(model="llama3-8b-8192"),  # Fastest model option
+        tts=cartesia.TTS(voice="248be419-c632-4f23-adf1-5324ed7dbf1d"),
         chat_ctx=initial_ctx,
+        fnc_ctx=fnc_ctx,
     )
-    assistant.start(ctx.room)
+    assitant.start(ctx.room)
 
     await asyncio.sleep(1)
-    await assistant.say("Hello! How can I help you today?", allow_interruptions=True)
+    await assitant.say("Hey, how can I help you today!", allow_interruptions=True)
+
 
 if __name__ == "__main__":
     cli.run_app(WorkerOptions(entrypoint_fnc=entrypoint))
